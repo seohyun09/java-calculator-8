852d9e574136ed4f036ef733637ea2e7b9a9e772
이서현 <mellifluous3380@gmail.com>
2025-10-17 03:57:53 +0900

    feat: 커스텀 구분자 문자열숫자 합 기능

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,6 +1,7 @@
 package calculator;
 
 import camp.nextstep.edu.missionutils.Console;
+import java.util.regex.Pattern;
 
 public class Application {
     public static void main(String[] args) {
@@ -9,13 +10,21 @@ public class Application {
 
         int sum = 0;
 
-        // 입력값 검증
         if (input.startsWith("//")) {
             int i = 2;
             StringBuilder separator = new StringBuilder();
             while (i < input.length() && input.charAt(i) != '\n') {
                 separator.append(input.charAt(i));
                 i++;
+            }
+            String numbers = input.substring(i + 1);
+            String[] tokens = numbers.split(Pattern.quote(separator.toString()));
+            for (String token : tokens) {
+                int num = Integer.parseInt(token);
+                if (num < 0) {
+                    throw new IllegalArgumentException();
+                }
+                sum += num;
             }
         } else if (Character.isDigit(input.charAt(0))) {
             String[] tokens = input.split(",|;");
