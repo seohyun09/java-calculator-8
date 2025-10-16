d2115c2b1de61e8f2d04bd5bc44a54f492ec2ed9
이서현 <mellifluous3380@gmail.com>
2025-10-17 03:27:59 +0900

    feat: 커스텀 ê입력값 ê: 커스텀 구분자 지정

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -6,5 +6,15 @@ public class Application {
     public static void main(String[] args) {
         System.out.println("덧셈할 문자열을 입력해 주세요.");
         String input = Console.readLine();
+
+        // 입력값 검증
+        if (input.startsWith("//")) {
+            int i = 2;
+            StringBuilder separator = new StringBuilder();
+            while (i < input.length() && input.charAt(i) != '\n') {
+                separator.append(input.charAt(i));
+                i++;
+            }
+        }
     }
 }
